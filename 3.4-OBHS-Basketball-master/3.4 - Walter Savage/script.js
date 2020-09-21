97978ef88238bfe9f892a2b52cad0d7d0a56a71c
Walter Savage <63981922+walterthesavage@users.noreply.github.com>
2020-09-22 11:47:33 +1200

    Add files via upload

---
+++ 3.4-OBHS-Basketball-master/3.4 - Walter Savage/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
